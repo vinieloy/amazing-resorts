21fd53b601853ac4fb8b38b5b25c491cb4ad0212
Vinicius Eloy <vinieloy@msn.com>
2016-05-20 01:07:04 -0300

    Adicionando service de veiculos

---
+++ app/services/veiculoService.js
@@ -0,0 +1,46 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app')
+    .service('veiculoService', veiculoService);
+
+  veiculoService.$inject = ['$http', 'Api'];
+
+  function veiculoService($http, Api) {
+
+    var vm = this;
+
+    vm.getAll = getAll;
+    vm.salvarVeiculo = salvarVeiculo;
+    vm.desativarVeiculo = desativarVeiculo;
+
+
+    function getAll() {
+      return $http.get(Api.Veiculo)
+        .success(success)
+        .error(error);
+    }
+
+    function salvarVeiculo(veiculo) {
+      return $http.post(Api.Veiculo, JSON.stringify(veiculo))
+        .success(success)
+        .error(error);
+    }
+
+    function desativarVeiculo(id) {
+      return $http.post(Api.DesativarVeiculo, id)
+        .success(success)
+        .error(error);
+    }
+
+    function success(response) {
+      return response;
+    }
+
+    function error(response) {
+      console.log('Error');
+    }
+
+  }
+})();
