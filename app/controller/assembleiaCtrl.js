2a93bc4f8315d71222ca0c6b8b5548fcc995db86
Vinicius Eloy <vinieloy@msn.com>
2016-05-22 21:05:52 -0300

    Ajustes de imovel, servicos e assembleia

--- app/controller/assembleiaCtrl.js
+++ app/controller/assembleiaCtrl.js
@@ -11,6 +11,7 @@
 
     var assem = this;
     var _selectedAssembleia = null;
+    var _selectedAta = null;
 
     assem.assembleias = null;
     assem.condominos = null;
@@ -20,8 +21,35 @@
       paginas: 1
     };
 
-    listarAssembleias();
+    assem.recarregar = recarregar;
+    assem.salvarAssembleia = salvarAssembleia;
+    assem.editarAssembleia = editarAssembleia;
+    assem.excluirAssembleia = excluirAssembleia;
+    assem.limparAssembleia = limparAssembleia;
+    assem.salvarAta = salvarAta;
+    assem.editarAta = editarAta;
+    assem.excluirAta = excluirAta;
+    assem.limparAta = limparAta;
 
+
+    init();
+
+
+    function init() {
+      listarAssembleias();
+      listarAta();
+    }
+
+
+    function recarregar() {
+      assem.assembleias = null;
+      assem.atas = null;
+      assem.loading = $timeout(function () {
+        listarAssembleias();
+      }, 1000);
+    }
+
+    // assembleia
     function listarAssembleias() {
       assembleiaService.getAll().then(success, error);
 
@@ -30,47 +58,7 @@
       };
     }
 
-    assem.recarregar = function () {
-      assem.assembleias = null;
-      assem.loading = $timeout(function () {
-        listarAssembleias();
-      }, 1000);
-    }
-
-    assem.editar = function (event, assembleia) {
-      assem.assembleias = null;
-      _selected = assembleia;
-      assem.formAssembleia = assembleia;
-      assem.selectedIndex = 1;
-    }
-
-    assem.excluir = function (event, assembleia) {
-
-      var confirm = $mdDialog.confirm()
-        .title('Excluir evento: "' + assembleia.titulo + '" ?')
-        .ok('Sim')
-        .cancel('Cancelar');
-
-      $mdDialog.show(confirm).then(function () {
-        assembleiaService.desativarPessoa(pessoa.id).then(success, error);
-      }, function () {
-        console.log('cancelou');
-      });
-    }
-
-    assem.limpar = function (event) {
-      assem.formAssembleia = null;
-    }
-
-    // assem.getCondominos = function(event) {
-    //     var service = usuarioService;
-    //     service.getAll()
-    //         .then(function(response) {
-    //             assem.condominos = response.data;
-    //         }, error);
-    // };
-
-    assem.salvar = function (event, assembleia) {
+    function salvarAssembleia(event, assembleia) {
 
       assembleiaService.salvarAssembleia(assembleia)
         .then(success, error);
@@ -89,6 +77,107 @@
       }
     }
 
+    function editarAssembleia(event, assembleia) {
+      assem.formAssembleia = null;
+      _selectedAssembleia = assembleia;
+      assem.formAssembleia = _selectedAssembleia;
+      assem.selectedIndex = 1;
+    }
+
+    function excluirAssembleia(event, assembleia) {
+
+      var confirm = $mdDialog.confirm()
+        .title('Excluir evento: "' + assembleia.titulo + '" ?')
+        .ok('Sim')
+        .cancel('Cancelar');
+
+      $mdDialog.show(confirm).then(function () {
+        assembleiaService.excluirAssembleia(assembleia.id).then(success, error);
+      }, function () {
+        console.log('cancelou');
+      });
+    }
+
+    function limparAssembleia(event) {
+      assem.formAssembleia = '';
+    }
+
+
+    // ata
+    function listarAta() {
+      assembleiaService.getAllAta().then(success, error);
+
+      function success(response) {
+        assem.atas = response.data;
+      };
+    }
+
+    function salvarAta(event, ata) {
+
+      assembleiaService.salvarAta(ata)
+        .then(success, error);
+
+      function success(response) {
+        assem.formAta = null;
+        listarAta();
+        assem.selectedIndex = 3;
+
+        $mdToast.show(
+          $mdToast.simple()
+          .textContent('Salvo com sucesso')
+          .position('top right')
+          .hideDelay(3000)
+        );
+      }
+    }
+
+    function editarAta(event, ata) {
+      assem.formAta = null;
+      _selectedAta = ata;
+      assem.formAta = _selectedAta;
+      assem.selectedIndex = 4;
+    }
+
+    function excluirAta(event, ata) {
+
+      var confirm = $mdDialog.confirm()
+        .title('Excluir ata: "' + ata.titulo + '" ?')
+        .ok('Sim')
+        .cancel('Cancelar');
+
+      $mdDialog.show(confirm).then(function () {
+        assembleiaService.excluirAta(ata.id).then(success, error);
+      }, function () {
+        console.log('cancelou');
+      });
+    }
+
+    function limparAta(event) {
+      assem.formAta = '';
+    }
+
+
+    function carregarAssembleia() {
+      // Use timeout to simulate a 650ms request.
+      return $timeout(function () {
+        pessoaService.getAllMoradores().then(success, error);
+
+        function success(response) {
+          ve.pessoas = response.data;
+        };
+      }, 650);
+    };
+
+    // assem.getCondominos = function(event) {
+    //     var service = usuarioService;
+    //     service.getAll()
+    //         .then(function(response) {
+    //             assem.condominos = response.data;
+    //         }, error);
+    // };
+
+
+
     function error(response) {
       $mdToast.show(
         $mdToast.simple()
@@ -97,19 +186,6 @@
         .hideDelay(3000)
       );
     }
-
-
-    assem.vegetables = ['Corn', 'Onions', 'Kale', 'Arugula', 'Peas', 'Zucchini'];
-    assem.searchTerm;
-    assem.clearSearchTerm = function () {
-      assem.searchTerm = '';
-    };
-    // The md-select directive eats keydown events for some quick select
-    // logic. Since we have a search input here, we don't need that logic.
-    $element.find('input.demo-header-searchbox').on('keydown', function (ev) {
-      ev.stopPropagation();
-    });
-
 
   }
 })();
