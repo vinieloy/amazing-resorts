81c65732293fbdc3fc8390a042ffad4b7571c004
Andr2d2 <santosandreoliveira@gmail.com>
2016-06-07 02:10:12 -0300

    charts

--- app/controller/relatoriosCtrl.js
+++ app/controller/relatoriosCtrl.js
@@ -1,16 +1,25 @@
-(function () {
+(function() {
   'use strict';
 
   angular
     .module('app')
     .controller('relatoriosCtrl', relatoriosCtrl);
 
-  relatoriosCtrl.$inject = ['$window', '$mdToast'];
+  relatoriosCtrl.$inject = [];
 
-  function relatoriosCtrl($window, $mdToast) {
+  function relatoriosCtrl() {
 
     var re = this;
+    re.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    re.series = ['Series A', 'Series B'];
 
+    re.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
 
+    re.labels2 = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
+    re.data2 = [300, 500, 100];
   }
+
 })();
--- app/modules/app.js
+++ app/modules/app.js
@@ -1,55 +1,57 @@
-var app = angular.module('app', ['ngMaterial', 'ui.router', 'md.data.table']);
+var app = angular.module('app', ['ngMaterial', 'ui.router', 'md.data.table', 'chart.js']);
 
 app.config(function name($mdThemingProvider) {
 
-  $mdThemingProvider.theme('default')
-    //.dark()
-    //.primaryPalette('purple')
-    //.accentPalette('orange')
-    //.warnPalette('red');
-  ;
+    $mdThemingProvider.theme('default')
+        //.dark()
+        //.primaryPalette('purple')
+        //.accentPalette('orange')
+        //.warnPalette('red');
+    ;
 });
 
+
+
 app.constant("Api", {
-  "url": "http://amazingresort.somee.com/api/",
-  
-  "Usuario": "http://amazingresort.somee.com/api/Usuario/",
-  "ExcluirUsuario": "http://amazingresort.somee.com/api/Usuario/ExcluirPessoa/",
-  
-  "Funcionario": "http://amazingresort.somee.com/api/Funcionario/",
-  
-  "Morador": "http://amazingresort.somee.com/api/Morador/",
-  "CriarMorador": "http://amazingresort.somee.com/api/Morador/CriarMorador/",
-  
-  "Imovel": "http://amazingresort.somee.com/api/Imovel/",
-  "CriarImovel": "http://amazingresort.somee.com/api/Imovel/CriarImovel/",
-  "ExcluirImovel": "http://amazingresort.somee.com/api/Imovel/ExcluirImovel/",
-  
-  "Veiculo": "http://amazingresort.somee.com/api/Veiculo/",
-  "CriarVeiculo": "http://amazingresort.somee.com/api/Veiculo/CriarVeiculo/",
-  "ExcluirVeiculo": "http://amazingresort.somee.com/api/Veiculo/ExcluirVeiculo/",
-  
-  "Servico": "http://amazingresort.somee.com/api/Servico/",
-  "CriarServico": "http://amazingresort.somee.com/api/Servico/CriarServico/",
-  "ExcluirServico": "http://amazingresort.somee.com/api/Servico/ExcluirServico/",
-  
-  "Assembleia": "http://amazingresort.somee.com/api/Assembleia/",
-  "CriarAssembleia": "http://amazingresort.somee.com/api/Assembleia/CriarAssembleia/",  
-  "ExcluirAssembleia": "http://amazingresort.somee.com/api/Assembleia/ExcluirAssembleia/",
-  
-  "Ata": "http://amazingresort.somee.com/api/Ata/",
-  "CriarAta": "http://amazingresort.somee.com/api/Ata/CriarAta/",  
-  "ExcluirAta": "http://amazingresort.somee.com/api/Ata/ExcluirAta/",
-  
-  "AtaParticipante": "http://amazingresort.somee.com/api/AtaParticipante/",
-  "CriarAtaParticipante": "http://amazingresort.somee.com/api/AtaParticipante/CriarAtaParticipante/",  
-  "ExcluirAtaParticipante": "http://amazingresort.somee.com/api/AtaParticipante/ExcluirAtaParticipante/",
-  
-  "Topico": "http://amazingresort.somee.com/api/Topico/",
-  "CriarTopico": "http://amazingresort.somee.com/api/Topico/CriarTopico/",  
-  "ExcluirTopico": "http://amazingresort.somee.com/api/Topico/ExcluirTopico/",
-  
-  "Comentario": "http://amazingresort.somee.com/api/Comentario/",
-  "CriarComentario": "http://amazingresort.somee.com/api/Comentario/CriarComentario/",  
-  "ExcluirComentario": "http://amazingresort.somee.com/api/Comentario/ExcluirComentario/"
+    "url": "http://amazingresort.somee.com/api/",
+
+    "Usuario": "http://amazingresort.somee.com/api/Usuario/",
+    "ExcluirUsuario": "http://amazingresort.somee.com/api/Usuario/ExcluirPessoa/",
+
+    "Funcionario": "http://amazingresort.somee.com/api/Funcionario/",
+
+    "Morador": "http://amazingresort.somee.com/api/Morador/",
+    "CriarMorador": "http://amazingresort.somee.com/api/Morador/CriarMorador/",
+
+    "Imovel": "http://amazingresort.somee.com/api/Imovel/",
+    "CriarImovel": "http://amazingresort.somee.com/api/Imovel/CriarImovel/",
+    "ExcluirImovel": "http://amazingresort.somee.com/api/Imovel/ExcluirImovel/",
+
+    "Veiculo": "http://amazingresort.somee.com/api/Veiculo/",
+    "CriarVeiculo": "http://amazingresort.somee.com/api/Veiculo/CriarVeiculo/",
+    "ExcluirVeiculo": "http://amazingresort.somee.com/api/Veiculo/ExcluirVeiculo/",
+
+    "Servico": "http://amazingresort.somee.com/api/Servico/",
+    "CriarServico": "http://amazingresort.somee.com/api/Servico/CriarServico/",
+    "ExcluirServico": "http://amazingresort.somee.com/api/Servico/ExcluirServico/",
+
+    "Assembleia": "http://amazingresort.somee.com/api/Assembleia/",
+    "CriarAssembleia": "http://amazingresort.somee.com/api/Assembleia/CriarAssembleia/",
+    "ExcluirAssembleia": "http://amazingresort.somee.com/api/Assembleia/ExcluirAssembleia/",
+
+    "Ata": "http://amazingresort.somee.com/api/Ata/",
+    "CriarAta": "http://amazingresort.somee.com/api/Ata/CriarAta/",
+    "ExcluirAta": "http://amazingresort.somee.com/api/Ata/ExcluirAta/",
+
+    "AtaParticipante": "http://amazingresort.somee.com/api/AtaParticipante/",
+    "CriarAtaParticipante": "http://amazingresort.somee.com/api/AtaParticipante/CriarAtaParticipante/",
+    "ExcluirAtaParticipante": "http://amazingresort.somee.com/api/AtaParticipante/ExcluirAtaParticipante/",
+
+    "Topico": "http://amazingresort.somee.com/api/Topico/",
+    "CriarTopico": "http://amazingresort.somee.com/api/Topico/CriarTopico/",
+    "ExcluirTopico": "http://amazingresort.somee.com/api/Topico/ExcluirTopico/",
+
+    "Comentario": "http://amazingresort.somee.com/api/Comentario/",
+    "CriarComentario": "http://amazingresort.somee.com/api/Comentario/CriarComentario/",
+    "ExcluirComentario": "http://amazingresort.somee.com/api/Comentario/ExcluirComentario/"
 });
